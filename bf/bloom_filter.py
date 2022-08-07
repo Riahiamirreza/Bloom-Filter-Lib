a7826e2e598645ceb7b7d3b3ca88779587e416dd
Amirreza <riahiamirreza79@gmail.com>
2022-08-07 22:42:43 +0430

    Added structure of BloomFilter class.

--- bf/bloom_filter.py
+++ bf/bloom_filter.py
@@ -1,3 +1,12 @@
+from typing import TypeVar
+
 
 class BloomFilter:
-    ...
+    def __init__():
+        ...
+    
+    def insert(self, item: bytes or str) -> None:
+        ...
+    
+    def contain(self, item: bytes or str) -> bool:
+        ...
